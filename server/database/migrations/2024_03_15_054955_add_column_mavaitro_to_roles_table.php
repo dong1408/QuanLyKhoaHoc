b12d7d00752b05d741817d15afc92f1197f62c77
dong1408 <dongden14082002@gmail.com>
2024-03-15 13:07:39 +0700

    chinh sua miragtion roles

--- server/database/migrations/2024_03_15_054955_add_column_mavaitro_to_roles_table.php
+++ server/database/migrations/2024_03_15_054955_add_column_mavaitro_to_roles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('roles', function (Blueprint $table) {
-            $table->string('mavaitro',255)->unique();
+            $table->string('mavaitro', 255);
         });
     }
 
